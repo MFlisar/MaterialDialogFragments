1366aa1e432f73561ffd0e3625d7c6f96239aca8
MichaelFlisar <mflisar.development@gmail.com>
2019-07-30 10:15:32 +0200

    fixed trySendResult filtering

--- dialogs/src/main/java/com/michaelflisar/dialogs/utils/DialogUtil.kt
+++ dialogs/src/main/java/com/michaelflisar/dialogs/utils/DialogUtil.kt
@@ -44,7 +44,7 @@ object DialogUtil {
         }
                 .filterNotNull()
                 .filter {
-                    excludedFilter?.invoke(it) ?: true
+                    excludedFilter?.invoke(it)?.not() ?: true
                 }
 
         for (c in allCallbacks) {
